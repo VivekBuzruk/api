d2ed8136937f1af4fb2e12df534d28ae3265aee6
Vivek Buzruk <vivek.buzruk@gmail.com>
2021-01-14 15:34:42 +0530

    In sync with actual call from UI

--- src/main/java/com/capitalone/dashboard/rest/PipelineController.java
+++ src/main/java/com/capitalone/dashboard/rest/PipelineController.java
@@ -5,11 +5,16 @@ import com.capitalone.dashboard.model.PipelineResponse;
 import com.capitalone.dashboard.request.PipelineSearchRequest;
 import com.capitalone.dashboard.service.PipelineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.types.ObjectId;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -24,7 +29,14 @@ public class PipelineController {
     }
 
     @RequestMapping(value = "/pipeline", method = GET, produces = APPLICATION_JSON_VALUE)
-    public Iterable<PipelineResponse> searchPipelines(@Valid PipelineSearchRequest searchRequest) throws HygieiaException {
-        return pipelineService.search(searchRequest);
-    }
+
+    public Iterable searchPipelines(@RequestParam String collectorItemId, @RequestParam String beginDate, @RequestParam String endDate) throws HygieiaException {
+        PipelineSearchRequest pipelineSearchRequest = new PipelineSearchRequest();
+        pipelineSearchRequest.setBeginDate(Long.valueOf(beginDate));
+        pipelineSearchRequest.setEndDate(Long.valueOf(endDate));
+        List collectorItemIdList = new ArrayList<>();
+        collectorItemIdList.add(new ObjectId(collectorItemId));
+        pipelineSearchRequest.setCollectorItemId(collectorItemIdList);
+        return pipelineService.search(pipelineSearchRequest);
+    } 
 }
