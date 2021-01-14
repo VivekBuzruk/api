533faf095eb5c8b3dd9d7c11896d87c1e68d57ff
Vivek Buzruk <vivek.buzruk@gmail.com>
2021-01-14 15:26:48 +0530

    my Local Changes == Vivek

--- run_component.sh
+++ run_component.sh
@@ -1,1 +1,4 @@
-java -jar target/api.jar --spring.config.location=C:\dwork\HybridFramework\api\application.properties -Djasypt.encryptor.password=hygieiasecret
+java -jar target/api.jar --spring.config.location=application.properties -Djasypt.encryptor.password=hygieiasecret
+# java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n -jar target/api.jar --spring.config.location=application.properties -Djasypt.encryptor.password=hygieiasecret
+# Working New
+# java -Xdebug -agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n -jar target/api.jar --spring.config.location=application.properties -Djasypt.encryptor.password=hygieiasecret
